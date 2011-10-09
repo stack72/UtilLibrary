782b9ce29281081ad7dc26c3ff65cbe8ee8ed89c
Paul Stack <paulstack@hotmail.com>
2011-10-09 12:49:13 +0100

    addition of an output directory for the build dlls

--- UtilLibrary/Util.ConfigManager/Properties/AssemblyInfo.cs
+++ UtilLibrary/Util.ConfigManager/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PaulStack.Utilities")]
+[assembly: AssemblyTitle("Util.ConfigManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PaulStack.Utilities")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("SDCubed")]
+[assembly: AssemblyProduct("Util.ConfigManager")]
+[assembly: AssemblyCopyright("Copyright © SDCubed 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
