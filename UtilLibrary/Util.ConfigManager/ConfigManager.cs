c96be0e776d097249539d5390938981e7cfe66bc
Paul Stack <paulstack@hotmail.com>
2011-10-09 12:44:44 +0100

    initial checkin of the utility library - this has te configManager in it

---
+++ UtilLibrary/Util.ConfigManager/ConfigManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Util.ConfigManager
+{
+    public interface IConfigManager
+    {
+        T GetAppSettingAs<T>(string propertyName) where T : struct;
+    }
+
+    public class ConfigManager: IConfigManager
+    {
+        public T GetAppSettingAs<T>(string propertyName) where T : struct
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException("propertyName");
+
+            var value = System.Configuration.ConfigurationManager.AppSettings.Get(propertyName);
+
+            T returnVal = default(T);
+            if (String.IsNullOrEmpty(value))
+                return returnVal;
+
+            TryParse(value, out returnVal);
+            return returnVal;
+        }
+
+        private static void TryParse<T>(string s, out T value)
+        {
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            try
+            {
+                value = (T)converter.ConvertFromString(s);
+                return;
+            }
+            catch
+            {
+                value = default(T);
+                return;
+            }
+        }
+    }
+}
